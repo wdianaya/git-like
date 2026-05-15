616b735f1f27921efd29235f2d42ec84308731c1
diana <yarmuhametova.deana@yandex.ru>
2026-05-15 21:19:09 +0700

    added help and init empty repo

---
+++ main.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+#include "exist_dir.h"
+
+#define MAX_INPUT 1024
+#define MAX_ARGS 64
+
+// получить абсолютный путь переданного объекта
+void get_repo_path(char *name, char **path) {
+    *path = realpath(name, NULL);
+}
+
+// инициализация репозитория
+void init_repository() {
+    if (directory_exists(".mygit")) {
+        printf("Repository already exist\n");
+        return;
+    }
+
+    mkdir(".mygit", 0755);
+    mkdir(".mygit/objects", 0755);
+    mkdir(".mygit/refs", 0755);
+    mkdir(".mygit/refs/heads", 0755);
+
+    FILE* head = fopen(".mygit/HEAD", "w");
+    fprintf(head, "ref: refs/heads/master\n");
+    fclose(head);
+
+    FILE *index = fopen(".mygit/index", "w");
+    fclose(index);
+
+    char dirname[] = ".mygit";
+
+    char* path;
+    get_repo_path(dirname, &path);
+    printf("Initialized empty Git repository in path:");
+    if (path == NULL) {
+        printf("[cannot find]");
+    } else {
+        printf("[%s]", path);
+        free(path);
+    }
+
+    printf("\n");
+}
+
+void parse_argument(char *line, char **func, char **args, int *arg_count) {
+    char* token = strtok(line, " ");
+    if (token == NULL) return;
+
+    *func = token;
+
+    while ((token = strtok(NULL, " ")) != NULL && *arg_count < MAX_ARGS) {
+        args[*arg_count] = token;
+        (*arg_count)++;
+    }
+}
+
+void show_help() {
+    printf("\nAvailable commands:\n");
+    printf("  init [path]    Initialize a new repository\n");
+    printf("  help           Show this help message\n");
+    printf("  exit           Exit the program\n\n");
+}
+
+int main() {
+
+    char buff[MAX_INPUT];
+    while (1) {
+        printf("mygit> ");
+
+
+        if (fgets(buff, sizeof(buff), stdin) == NULL) {
+            break;
+        }
+
+        buff[strcspn(buff, "\n")] = '\0';
+        if (buff[0] == '\0') continue;
+
+        char* command;
+        char* args[MAX_ARGS];
+        int count_args = 0;
+
+        char buff_copy[MAX_INPUT];
+        strcpy(buff_copy, buff);
+        parse_argument(buff_copy, &command, args, &count_args);
+
+        if (command != NULL && strcmp(command, "help") == 0) {
+            show_help();
+        }
+        
+        else if (command != NULL && strcmp(command, "exit") == 0) {
+            break;
+        }
+
+        else if (command != NULL && strcmp(command, "init") == 0) {
+            init_repository();
+            continue;
+        }
+
+        else {
+            printf("Unknown command\n");
+
+        }
+    }
+
+    return 0;
+}
