6ecf66ef0876c52921d2732b10bfe9273f7269f8
diana <diana.yarmykh14@list.ru>
2026-06-01 03:03:16 +0700

    first

--- src/sha1.c
+++ src/sha1.c
@@ -11,7 +11,6 @@ void compute_sha1(const unsigned char *data, size_t len, char hash_hex[41]) {
     unsigned char hash[LEN_SHA];
 
     SHA1(data, len, hash);
-
     // преобразуем бинарный хеш в hex строку
     for (int i = 0; i < LEN_SHA; i++) {
         sprintf(hash_hex + (i * 2), "%02x", hash[i]);
