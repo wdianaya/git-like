8c1abed62973f5b7c885c1921ffb6a8cbe29b4fa
wdianaya <151443526+wdianaya@users.noreply.github.com>
2026-06-03 09:44:11 +0700

    Update log.c

--- src/log.c
+++ src/log.c
@@ -19,29 +19,56 @@ int read_commit(const char *hash, CommitInfo *commit) {
 
     while (fgets(line, sizeof(line), f)) {
         if (strncmp(line, "parent:", 7) == 0) {
-            sscanf(line,
-                   "parent: %40s",
-                   commit->parent);
+            sscanf(line, "parent: %40s", commit->parent);
         }
         else if (strncmp(line, "date:", 5) == 0) {
-            sscanf(line,
-                   "date: %[^\n]",
-                   commit->date);
+            sscanf(line, "date: %[^\n]", commit->date);
         }
         else if (strncmp(line, "message:", 8) == 0) {
-            sscanf(line,
-                   "message: %[^\n]",
-                   commit->message);
+            sscanf(line, "message: %[^\n]", commit->message);
         }
     }
 
     fclose(f);
-
     return 1;
+}
+
+void log_range(const char *from, const char *to) {
+    char current[41];
+
+    strcpy(current, to);
+
+    while (strcmp(current, "NULL") != 0) {
+        CommitInfo commit;
+        if (!read_commit(current, &commit)) {
+            break;
+        }
+
+        printf("\ncommit %s\n", commit.hash);
+        printf("Date: %s\n", commit.date);
+        printf("Message: %s\n", commit.message);
+
+        if (strcmp(current, from) == 0) {
+            break;
+        }
+        strcpy(current, commit.parent);
+    }
+}
+
+static char* get_hash(char* addr) {
+    char* temp = resolve_commit(addr);
+    if (!temp) {
+        fprintf(stderr, "commit or branch '%s' not found\n", addr);
+        return NULL;
+    }
+    return temp;
 }
 
 void log_command(char **args, int count) {
     char *start_commit = NULL;
+    char *range_start = NULL;
+    char *range_end = NULL;
+    int range_flag = 0;
     int limit = INT_MAX;
 
     // parse args
@@ -49,7 +76,16 @@ void log_command(char **args, int count) {
         case 0:
             break;
         case 1:
-            start_commit = args[0];
+            char *dots = strstr(args[0], "..");
+            if (dots) {
+                *dots = '\0';
+                range_start = get_hash(args[0]);
+                range_end = get_hash(dots + 2);
+                range_flag = 1;
+            }
+            else {
+                start_commit = get_hash(args[0]);
+            }
             break;
         case 2:
             if (strcmp(args[0], "--n") == 0) {
@@ -61,7 +97,7 @@ void log_command(char **args, int count) {
             break;
         case 3:
             if (strcmp(args[1], "--n") == 0) {
-                start_commit = args[0];
+                start_commit = get_hash(args[0]);
                 limit = atoi(args[2]);
             } else {
                 fprintf(stderr, "uncorrect arguments passed\n");
@@ -71,6 +107,26 @@ void log_command(char **args, int count) {
         default:
             fprintf(stderr, "many arguments passed\n");
             return;
+    }
+
+    if (range_start && range_end) {
+        CommitInfo tmp;
+
+        if (!read_commit(range_start, &tmp)) {
+            fprintf(stderr, "commit %s not found\n", range_start);
+            return;
+        }
+
+        if (!read_commit(range_end, &tmp)) {
+            fprintf(stderr, "commit %s not found\n", range_end);
+            return;
+        }
+
+        log_range(range_start, range_end);
+        return;
+    } else if (range_flag) {
+        fprintf(stderr, "for range write <hash1>..<hash2>\n");
+        return;
     }
 
     // если commit не указан
